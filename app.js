bbc15c0c3f1d86e9afc2c57b71ab60d26f2254f8
Salvo Russo <s.russo@prismaprogetti.it>
2020-11-01 17:41:37 +0100

    input values

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ import commandLineArgs from './utils/argsReader.js'
 import fileReader from './utils/fileReader.js'
 
 const args = commandLineArgs.readCommandLineArgs()
+const map = fileReader.readJson(args.map)
+const startId = args.start_room
+const toCollect = args.objects_to_collect
