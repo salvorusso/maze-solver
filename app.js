95e49eccbfe28cd93dff723fc0934dbd7c780156
Salvo Russo <s.russo@prismaprogetti.it>
2020-11-01 18:43:58 +0100

    first version

--- app.js
+++ app.js
@@ -5,3 +5,29 @@ const args = commandLineArgs.readCommandLineArgs()
 const map = fileReader.readJson(args.map)
 const startId = args.start_room
 const toCollect = args.objects_to_collect
+solveMaze()
+
+/**
+ * @param {Number} roomId 
+ * Retrieve the objects in that room
+ */
+function collect ( roomId ) {
+    const currentRoom = map.rooms.filter( room => room.id === roomId )[0]
+    let collected = []
+
+    if ( currentRoom.objects.length > 0 ){
+        currentRoom.objects.forEach(obj => {
+            collected.push(obj.name)
+        });
+    }
+    else
+        collected = 'None'
+
+    console.log( roomId + "  " + currentRoom.name + "  " + collected)
+
+}
+
+function solveMaze () {
+    console.log("ID  Room  Object")
+    collect(startId)
+}
