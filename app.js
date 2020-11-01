f36b2fe1583199523ccd98413f85eb4f1b924054
Salvo Russo <s.russo@prismaprogetti.it>
2020-11-01 16:48:01 +0100

    arg reader

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-// main
+import commandLineArgs from 'command-line-args'
+
+//Read command line args
+const argsDefinitions = [
+    { name: 'map', alias: 'm', type: String },
+    { name: "start_room", alias: 's', type: Number },
+    { name: "objects_to_collect", alias: 'o',  multiple: true, type: String }
+]
+const args = commandLineArgs(argsDefinitions)
